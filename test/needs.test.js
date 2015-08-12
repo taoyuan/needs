ae54213ef5aff75ef5cc7319a4d9bf5786661fec
taoyuan <torworx@gmail.com>
2015-08-12 13:32:19 +0800

    fixed #1

--- test/needs.test.js
+++ test/needs.test.js
@@ -6,125 +6,145 @@ var needs = require('..');
 var root = __dirname + '/dirs';
 
 describe('needs', function () {
-    it('requiring flatten directory', function () {
-        var controllers = needs(root, 'controllers', {
-            includes: '*Controller.js'
-        });
-        t.deepEqual(controllers, {
-            'main-Controller': {
-                index: 1,
-                show: 2,
-                add: 3,
-                edit: 4
-            },
+  it('requiring flatten directory', function () {
+    var controllers = needs(root, 'controllers', {
+      includes: '*Controller.js'
+    });
+    t.deepEqual(controllers, {
+      'main-Controller': {
+        index: 1,
+        show: 2,
+        add: 3,
+        edit: 4
+      },
 
-            'other-Controller': {
-                index: 1,
-                show: 'nothing'
-            }
-        });
+      'other-Controller': {
+        index: 1,
+        show: 'nothing'
+      }
+    });
+  });
+
+  it('requiring RegExp patterns', function () {
+    var controllers = needs(root, 'controllers', {
+      includes: /(.+Controller)\.js$/,
+      excludes: /^\.(git|svn)$/
+    });
+    t.deepEqual(controllers, {
+      'main-Controller': {
+        index: 1,
+        show: 2,
+        add: 3,
+        edit: 4
+      },
+
+      'other-Controller': {
+        index: 1,
+        show: 'nothing'
+      }
+    });
+  });
+
+  it('requiring json only became an option', function () {
+    var mydir = needs(root, 'mydir', {
+      includes: '+(*.js|*.json)'
     });
 
-    it('requiring json only became an option', function () {
-        var mydir = needs(root, 'mydir', {
-            includes: '+(*.js|*.json)'
-        });
+    var mydir_contents = {
+      foo: 'bar',
+      hello: {
+        world: true,
+        universe: 42
+      },
+      config: {
+        settingA: 'A',
+        settingB: 'B'
+      },
+      yes: true
+    };
+    t.deepEqual(mydir, mydir_contents);
 
-        var mydir_contents = {
-            foo: 'bar',
-            hello: {
-                world: true,
-                universe: 42
-            },
-            config: {
-                settingA: 'A',
-                settingB: 'B'
-            },
-            yes: true
-        };
-        t.deepEqual(mydir, mydir_contents);
+    var defaults = needs(root, 'mydir');
 
-        var defaults = needs(root, 'mydir');
+    t.deepEqual(defaults, mydir_contents);
+  });
 
-        t.deepEqual(defaults, mydir_contents);
+  it('requiring with excludes', function () {
+    var unfiltered = needs(root, 'filterdir', {
+      includes: '*.js'
     });
 
-    it('requiring with excludes', function () {
-        var unfiltered = needs(root, 'filterdir', {
-            includes: '*.js'
-        });
+    t.ok(unfiltered.root);
+    t.ok(unfiltered.hello);
 
-        t.ok(unfiltered.root);
-        t.ok(unfiltered.hello);
-
-        var excludedSub = needs(root, 'filterdir', {
-            includes: '*.js',
-            excludes: 'sub'
-        });
-
-        t.ok(excludedSub.root);
-        t.equal(excludedSub.hello, undefined);
+    var excludedSub = needs(root, 'filterdir', {
+      includes: '*.js',
+      excludes: 'sub'
     });
 
-    it('requiring common module file', function () {
-        var modules = needs(root, 'mydir', { module: 'config.json' });
-        t.deepEqual(modules, {
-            sub: {
-                settingA: 'A',
-                settingB: 'B'
-            }
-        });
+    t.ok(excludedSub.root);
+    t.equal(excludedSub.hello, undefined);
+  });
+
+  it('requiring common module file', function () {
+    var modules = needs(root, 'mydir', {module: 'config.json'});
+    t.deepEqual(modules, {
+      sub: {
+        settingA: 'A',
+        settingB: 'B'
+      }
+    });
+  });
+
+  it('requiring node modules', function () {
+    var modules = needs(root, 'modules', {module: true});
+    t.deepEqual(modules, {
+      foo: 'bar',
+      hello: {
+        world: true,
+        universe: 42
+      }
+    });
+  });
+
+  it('requiring modules with one parameter filter', function () {
+    var modules = needs(root, 'modules', {
+      module: true,
+      filter: function (info) {
+        var mod = require(info.file);
+        if (typeof mod === 'object') mod.moduleName = info.name;
+        return mod;
+      }
     });
 
-    it('requiring node modules', function () {
-        var modules = needs(root, 'modules', { module: true });
-        t.deepEqual(modules, {
-            foo: 'bar',
-            hello: {
-                world: true,
-                universe: 42
-            }
-        });
+    t.deepEqual(modules, {
+      foo: 'bar',
+      hello: {
+        world: true,
+        universe: 42,
+        moduleName: "hello"
+      }
+    });
+  });
+
+  it('requiring modules with two parameter filter', function () {
+    var modules = needs(root, 'modules', {
+      module: true,
+      filter: function (result, info) {
+        var mod = require(info.file);
+        if (typeof mod === 'object') mod.moduleName = info.name;
+        result[info.name] = mod;
+      }
     });
 
-    it('requiring modules with one parameter filter', function () {
-        var modules = needs(root, 'modules', {
-            module: true,
-            filter: function (info) {
-                var mod = require(info.file);
-                if (typeof mod === 'object') mod.moduleName = info.name;
-                return mod;
-            }
-        });
-
-        t.deepEqual(modules, {
-            foo: 'bar',
-            hello: {
-                world: true,
-                universe: 42,
-                moduleName: "hello"
-            }
-        });
+    t.deepEqual(modules, {
+      foo: 'bar',
+      hello: {
+        world: true,
+        universe: 42,
+        moduleName: "hello"
+      }
     });
-
-    it('requiring modules with two parameter filter', function () {
-        var modules = needs(root, 'modules', {
-            module: true,
-            filter: function (result, info) {
-                var mod = require(info.file);
-                if (typeof mod === 'object') mod.moduleName = info.name;
-                result[info.name] = mod;
-            }
-        });
-
-        t.deepEqual(modules, {
-            foo: 'bar',
-            hello: {
-                world: true,
-                universe: 42,
-                moduleName: "hello"
-            }
-        });
-    });
+  });
 
 });
